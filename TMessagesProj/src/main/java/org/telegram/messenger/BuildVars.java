335927173d454284e82e5f36ea0ad668b4e486b1
mesnik777end <abobuskakaka@gmail.com>
2026-06-24 18:44:57 +0200

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -25,16 +25,16 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
 
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 37940480;
+    public static String APP_HASH = "c8b4e886c5272175dbb7524fc8dd4";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
     public static int BUILD_VERSION; // generated
     public static String GITHUB_RELEASE_URL = "https://github.com/NextAlone/Nagram/releases";
 
-    public static int OFFICAL_APP_ID = 4;
-    public static String OFFICAL_APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int OFFICAL_APP_ID = 37940480;
+    public static String OFFICAL_APP_HASH = "c8b4e886c5272175dbb7524fc8dd4";
 
     public static int TGX_APP_ID = 21724;
     public static String TGX_APP_HASH = "3e0cb5efcd52300aec5994fdfc5bdc16";
